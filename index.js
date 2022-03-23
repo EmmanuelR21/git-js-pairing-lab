4228ec1c8a40ca1b7ef1381f2ac8fb08e8d2f207
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-03-23 19:51:42 +0000

    All done except 15

--- index.js
+++ index.js
@@ -151,3 +151,18 @@ function switchLetters(string) {
     
     
 }
+
+//problem 16
+
+function changeString(string) {
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    let newString = [];
+    
+    for(let i = 0; i < string.length; i++) {
+        let temp = alphabet.indexOf(string[i]) + 1;
+        newString.push(alphabet[temp]);
+    }
+    console.log(newString.join(''));
+}
+
+changeString("hello")
