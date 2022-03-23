8a4bfeaca64e7ba5a9a27b111bf1840d0e6fb6f9
aaronminaya <aaronminaya0@gmail.com>
2022-03-23 18:01:47 +0000

    second problem done

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 //Code your solutions in this file
+// problem 1
 function fiveToHundred() {
     for(let i = 5; i <= 100; i++) {
         console.log(i);
     }
 }
 
-fiveToHundred();
+//fiveToHundred();
+
+//problem 2
+
+function multiplesOfThree(){
+    for (let i = 0; i <= 100; i++){
+        if (i % 3 === 0){
+            console.log(i)
+        }
+    }
+}
+
+//multiplesOfThree();
